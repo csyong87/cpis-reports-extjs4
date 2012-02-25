f9e8f001b1d3c3ad1cbdc8ca1951d1e43fd73fd3
unknown <cjjavellana@gmail.com>
2012-02-25 15:37:09 +0800

    Added issuesClosed field to Division model; Updated issues-received

--- cpisapp/controller/numissuesreceived/NumIssuesReceived.js
+++ cpisapp/controller/numissuesreceived/NumIssuesReceived.js
@@ -15,16 +15,16 @@ Ext.define('CPIS.controller.numissuesreceived.NumIssuesReceived', {
 		selector : 'numissuesreceivedsearchform combobox[name=year]',
 		ref : 'selYear'
 	}, {
-		selector: 'issuesreceivedchart',
-		ref: 'stackedBarChart'
-	}, {
 		selector: 'viewport > panel',
 		ref: 'viewPort'
-	}, {
-        selector: '#issuesreceivedtabledata',
-        ref: 'tableData'
-    }],
+	}],
+    
+    // ~ Store definitions ===================================
+    
 	stores : ['Divisions', 'IssuesReceived', 'Months', 'Years'],
+    
+    // ~ Initialization methods =========================================
+    
 	onLaunch : function() {
 		var monthsStore = this.getMonthsStore();
 		monthsStore.load();
@@ -49,6 +49,9 @@ Ext.define('CPIS.controller.numissuesreceived.NumIssuesReceived', {
 			}
 		});
 	},
+    
+    // ~ Callback handler methods ===================================================
+    
 	onNumIssuesReceivedSearch : function(selModel, selection) {
 		var issuesReceivedStore = this.getIssuesReceivedStore();
 		issuesReceivedStore.load();
@@ -59,7 +62,7 @@ Ext.define('CPIS.controller.numissuesreceived.NumIssuesReceived', {
 	 * Called whenever IssuesReceived store is refreshed.
 	 * 
 	 * Transforms the data from a nested model (IssuesReceived <- Division) to a
-	 * flat model and
+	 * flat model
 	 * 
      * - Do i care to move the content of this method to a helper class? Lols
      * 
@@ -71,25 +74,64 @@ Ext.define('CPIS.controller.numissuesreceived.NumIssuesReceived', {
 	 *            options
 	 */
 	onIssueReceivedStoreLoad : function(store, records, options) {
-		var fields = []; //used to configure the fields of the dynamic model
 		var data = []; //Holds the chart's data
 		var data_fields = []; //Since a javascript object property cannot have spaces, we need to mask it
         var legend = []; //Holds the chart's legend array
-       
+        
 		var hasIdentifiedFields = false;
         var fieldAliasToDisplayName = new Object();
         
-		fields.push({name: 'categoryname', type: 'String'});
-		store.each(function(issuecategory) {
+        // the array used to hold the field definitions for the dynamic model used by the issues received chart 
+		var fields = [{name: 'categoryname', type: 'String'}];
+        
+        // the array used to hold the field definitions for the dynamic model used by the issues received summary
+        var summary_fields = [{name: 'division', type: 'String'}, {name: 'issuesClosed', type: 'int'}];
+        
+        //
+        //{division, total_1, total_2, total_3}
+        //
+        var summary_data = [];
+		store.each(function(issuecategory, categoryIdx) {
 			var obj = new Object();
 			obj['categoryname'] = issuecategory.data.categoryname;
+            
+            var categoryIdxField = 'category_' + categoryIdx;
+            summary_fields.push({name: categoryIdxField, type: 'String'});
+            
+           
 			Ext.each(issuecategory.raw.divisions, function(data, index) {
                 // define the field alias
                 var fieldname = 'data_idx_' + index;
 			    
                 obj[fieldname] = data.issueCount;
                 
-			    if(!hasIdentifiedFields){
+                //check if we have an instance of the division
+                var summaryDataFound = -1;
+                for(var i = 0; i < summary_data.length; i++) {
+                    if(summary_data[i].division == data.divisionName) {
+                        summaryDataFound = i;
+                        break;
+                    }
+                }                
+                
+                if(summaryDataFound > -1) {
+                    //we have an instance of the division
+                    //cool, we just have to pull up that record, add up the issues closed
+                    //and create a new field for the new category and assign the issue count
+                    var summaryObj = summary_data[i];
+                    summaryObj['issuesClosed'] =  summaryObj['issuesClosed'] + data.issuesClosed;
+                    summaryObj[categoryIdxField] = data.issueCount;
+                   
+                } else {
+                    //we dont have an instance of the division yet; We have to create
+                    var summaryObj = new Object();
+                    summaryObj['division'] = data.divisionName;
+                    summaryObj[categoryIdxField] = data.issueCount;
+                    summaryObj['issuesClosed'] = data.issuesClosed;
+                    summary_data.push(summaryObj);
+                }
+                
+                if(!hasIdentifiedFields){
 			    	fields.push({name : fieldname, type: 'String'});
                     legend.push(data.divisionName);
                     fieldAliasToDisplayName[fieldname] = data.divisionName;
@@ -99,13 +141,16 @@ Ext.define('CPIS.controller.numissuesreceived.NumIssuesReceived', {
 			    for(var i = 0; i < data_fields.length; i++) {
 			    	if(fieldname == data_fields[i]) {
 			    		isNew = false;
+                        break;
 			    	}
 			    }
 			    
 			    if(isNew){
 			    	data_fields.push(fieldname);
-                    
 			    }
+                
+                //issues received summary section
+                
 			});
 			
 			hasIdentifiedFields = true;
@@ -113,17 +158,25 @@ Ext.define('CPIS.controller.numissuesreceived.NumIssuesReceived', {
 			data.push(obj);
 		});
 		
+        console.log(summary_data);
+        
+        // ~ Dynamic model definitions ============================
+        
 		var model = Ext.define('DynamicModel', {
 			extend : 'Ext.data.Model',
 			fields: fields
 		});
 		
-		var dataWrapper = new Object();
-		dataWrapper['data'] = data;
-		
-		//dynamic store
-		var dynaStore = Ext.define('DynaStore', {
-			extend: 'Ext.data.Store',
+        var summaryModel = Ext.define('SummaryModel', {
+            extend: 'Ext.data.Model', 
+            fields: summary_fields
+        });
+        
+        // ~ Dynamic store instances ==================================
+        var dataWrapper = new Object();
+        dataWrapper['data'] = data;
+        
+		var dynaStore = Ext.create('Ext.data.Store', {
 			model: model,
 			require: model,
 		    data: dataWrapper, 
@@ -136,10 +189,29 @@ Ext.define('CPIS.controller.numissuesreceived.NumIssuesReceived', {
 		    }
 		});
 		
-		dynaStore = new dynaStore();
-		var stackedBar = Ext.define('StackedBar', {
-			extend: 'Ext.chart.Chart',
+      
+        
+        //Retrieve the reference of the chart and data grid
+        var tableDataRef = Ext.getCmp('issuesreceivedtabledata');
+        var issuesReceivedChartRef = Ext.getCmp('issuesreceivedchart')
+        
+        var viewPort = this.getViewPort();
+        
+        // ~ Dynamic component instances ==================================
+        
+        //check if we have a valid reference of the chart and data grid
+        //if we have, unregister it from the component manager because
+        //we want to create a new component of the same id
+        if(tableDataRef && issuesReceivedChartRef){
+            Ext.ComponentManager.unregister(tableDataRef);
+            Ext.ComponentManager.unregister(issuesReceivedChartRef);
+            viewPort.remove(tableDataRef);
+            viewPort.remove(issuesReceivedChartRef);
+        }
+        
+		var stackedBar = Ext.create('Ext.chart.Chart', {
 			alias: 'widget.issuesreceivedchart',
+            id: 'issuesreceivedchart',
 			animate : true,
 			store : dynaStore,
 			width : 800,
@@ -188,53 +260,46 @@ Ext.define('CPIS.controller.numissuesreceived.NumIssuesReceived', {
 		
         var columns = []; //Holds the dynamic table's columns
         
-        //iterate through the data records
-        Ext.each(data, function(data_obj, index){
-            
-            //define the first column
-            var category_column = {
-                text: 'Issue Category',
-                width: 298,
+        //define the first column
+        var category_column = {
+            text: 'Issue Category',
+            width: 298,
+            sortable: false,
+            hideable: false,
+            dataIndex: 'categoryname'
+        };
+        
+        //The rest of the columns
+        var statistics_column = [];
+        for (var field in fieldAliasToDisplayName){
+            statistics_column.push({
+                header: fieldAliasToDisplayName[field],
+                width: 100,
                 sortable: false,
                 hideable: false,
-                dataIndex: 'categoryname'
-            };
-            
-            //the dynamic columns
-            var statistics_column = [];
-            for (var field in fieldAliasToDisplayName){
-                statistics_column.push({
-                    text: fieldAliasToDisplayName[field],
-                    width: 100,
-                    sortable: false,
-                    hideable: false,
-                    dataIndex: field
-                });
-            }
-            
-            columns.push([
-                category_column,
-                statistics_column
-            ]);
-            
-            return false;
-        });
+                dataIndex: field,
+                field: {
+	                xtype: 'numberfield'
+	            }
+            });
+        }
         
-        console.log(columns);
-        
+        columns.push([
+            category_column,
+            statistics_column
+        ]);
+            
         var tableData = Ext.create('Ext.grid.Panel', {
-            id: 'issuesreceivedtabledata',
 		    store: dynaStore,
+            id: 'issuesreceivedtabledata',
 		    width: '100%',
 		    height: 200,
 		    title: 'Issues Received by Division',
 		    columns: columns
 		});
         
-		var viewPort = this.getViewPort();
-        viewPort.remove(this.getStackedBarChart());
-        viewPort.remove(this.getTableData());
-		viewPort.add(new stackedBar());
+      
+		viewPort.add(stackedBar);
 		viewPort.add(tableData);
 		
 	}
--- cpisapp/model/Division.js
+++ cpisapp/model/Division.js
@@ -1,5 +1,7 @@
 /**
- * This class represents a Division
+ * This class represents a CPIS Division. 
+ * 
+ * This file is part of the CPIS reporting module.
  */
 Ext.define('CPIS.model.Division', {
 	extend : 'Ext.data.Model',
@@ -12,7 +14,10 @@ Ext.define('CPIS.model.Division', {
 	}, {
 		name : 'issueCount',
 		type : 'int'
-	} ],
+	}, {
+        name: 'issuesClosed',
+        type : 'int'
+    }],
 	associations : [ {
 		type : 'belongsTo',
 		model : 'CPIS.model.IssueCategory'
