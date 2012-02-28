7f01ad51e158253cfd6f5b1532c3c2fef7112dce
Christian Javellana <cjjavellana@gmail.com>
2012-02-28 15:13:07 +0800

    Added copyright to source files

--- cpisapp/controller/top5category/Top5Category.js
+++ cpisapp/controller/top5category/Top5Category.js
@@ -1,3 +1,12 @@
+/*
+ * This file is part of the CPIS application.
+ * 
+ * Copyright (c) 2012 Mahindra Satyam
+ * 
+ * GNU General Public License Usage
+ * This file may be used under the terms of the GNU General Public License version 3.0 as published by the Free Software Foundation and appearing in the file LICENSE included in the packaging of this file.  Please review the following information to ensure the GNU General Public License version 3.0 requirements will be met: http://www.gnu.org/copyleft/gpl.html.
+ * 
+ */
 /**
  * This controller handles events / interactions related to the Top5Category
  * Charts
--- cpisapp/model/IssueByCategory.js
+++ cpisapp/model/IssueByCategory.js
@@ -1,3 +1,17 @@
+/*
+ * This file is part of the CPIS application.
+ * 
+ * Copyright (c) 2012 Mahindra Satyam
+ * 
+ * GNU General Public License Usage
+ * This file may be used under the terms of the GNU General Public License version 3.0 as published by the Free Software Foundation and appearing in the file LICENSE included in the packaging of this file.  Please review the following information to ensure the GNU General Public License version 3.0 requirements will be met: http://www.gnu.org/copyleft/gpl.html.
+ * 
+ */
+/**
+ * This class represents an issue count by category
+ * 
+ * @author Christian
+ */
 Ext.define('CPIS.model.IssueByCategory', {
 	extend: 'Ext.data.Model',
 	fields: [
